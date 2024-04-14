7f2f56f31ec97684591bf1f5f6221aa39aedee35
LOKESH RAM <90214872+Lokeshram12@users.noreply.github.com>
2024-04-14 12:53:06 +0000

    made my first basic nodeJs server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log(`welcome to backend development`);
+require('dotenv').config()
+
+const express = require('express') //
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/loke', (req, res) => {
+    res.send('Hello lokesh')
+  })
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
